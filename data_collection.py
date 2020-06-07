ed136df00117a7b8623e1f00bffb479e25effe87
Mario <mariocanedit1@gmail.com>
2020-06-07 08:34:57 -0400

    added win version of scrapper and chrome driver, added scrapped dataset

--- data_collection.py
+++ data_collection.py
@@ -9,11 +9,11 @@ Created on Fri Jun  5 22:25:25 2020
 
 import glassdoor_scraper as gs
 import pandas as pd
-path = "/Users/mtss/Documents/cs_salary_proj/chromedriver"
+path = "/Users/Mario/Documents/DS_Projects/cs_salary_proj/chromedriver"
 
 # Scrapes 20000 job listings for each title from glassdoor and saves them to dataframes
-df = gs.get_jobs('software engineer', 20000, False, path, 15)
+df = gs.get_jobs('software engineer', 4000, False, path, 15)
 
 
 # Stores all dataframes into .CSV files
-df_brand_amb.to_csv('glassdoor_jobs',index = False)
+df.to_csv('software_engineer_jobs',index = False)
