60ca4f5c071aa68881c632e4cc373cbb3eb1da8f
MTSS <mtss@MTSSs-MBP.domain>
2020-06-16 10:21:17 -0400

    cleaned up model deployment code, fixed model code to include job state and seniority status

--- Model_Deployment/cs_model_deploy.py
+++ Model_Deployment/cs_model_deploy.py
@@ -1,68 +1,54 @@
 # -*- coding: utf-8 -*-
 
+import pickle
 import pandas as pd
-
-df = pd.read_csv('cs_data_cleaned.csv')
-
-df['avg_salary'] = df.apply(lambda x: x.avg_salary*1000, axis =1)
-
-df_model = df[['avg_salary','Rating','Size','Type of ownership','Industry','Sector','Revenue',
-             'job_state','same_state','age','python_yn','java_yn','C_plus_plus_yn',
-             'C_sharp_yn','PHP_yn','swift_yn','ruby_yn','javascript_yn','SQL_yn','job_simp','seniority']]
-
-# get dummy data to convert categorical variable into dummy/indicator variables
-df_dum = pd.get_dummies(df_model)
-df_dum
-
-df_real = df[["avg_salary", "python_yn","java_yn","C_plus_plus_yn",
-             "C_sharp_yn","PHP_yn","swift_yn","ruby_yn","javascript_yn","SQL_yn"]]
-
-
-"""# Train and Test Split (80/20)"""
+import numpy as np
 from sklearn.model_selection import train_test_split
-
-X = df_real.drop('avg_salary', axis = 1)
-X
-y = df_real.avg_salary
-y.value_counts()
-
-# 80/20 train test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
-
-# Model Building (Random Forrest)
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import cross_val_score
-import numpy as np
-
-rf = RandomForestRegressor(max_depth=2, random_state=0)
-
-# Tuning Random Forest using GridSearch
 from sklearn.model_selection import GridSearchCV
 
-params = {'n_estimators':range(10,100,10), 'criterion':('mse','mae'), 'max_features':('auto','sqrt','log2')}
+# Import data
+df = pd.read_csv('cs_data_cleaned.csv')
+
+df['avg_salary'] = df.apply(lambda x: x.avg_salary*1000, axis = 1)
+df['senior_yn'] = df['seniority'].apply(lambda x: 1 if 'senior' in x.lower() else 0)
+
+df_model = df[['avg_salary', 'job_state', 'python_yn', 'java_yn', 'C_plus_plus_yn',
+             'C_sharp_yn', 'PHP_yn', 'swift_yn', 'ruby_yn', 'javascript_yn', 'SQL_yn',
+             'senior_yn']]
+
+# get dummy data to convert categorical variable into dummy/indicator variables
+df_dum = pd.get_dummies(df_model)
+
+
+## Model Building (Random Forrest) ##
+
+X = df_dum.drop('avg_salary', axis = 1)
+y = df_dum.avg_salary.values
+
+# Train and Test Split (80/20)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
+
+rf = RandomForestRegressor(max_depth = 2, random_state = 0)
+np.mean(cross_val_score(rf, X_train, y_train, scoring = 'neg_mean_absolute_error', cv= 10))
+
+# Tuning Random Forest using GridSearch
+params = {'n_estimators':range(10, 100, 10), 'criterion':('mse','mae'), 'max_features':('auto','sqrt','log2')}
 
 gs_rf = GridSearchCV(rf, params ,scoring = 'neg_mean_absolute_error', cv = 10)
 gs_rf.fit(X_train, y_train)
 
-gs_rf.best_score_
+gs_rf.best_score_  # MAE of -16853.64387558424
 best_model = gs_rf.best_estimator_
 
-
-
-
-import pickle
-
-# save the model to disk
+# Pickle / save model
 with open('model.pkl', 'wb') as file:
     pickle.dump(best_model, file)
 
 
-#pickle.dump(xgb_best, open('model.pkl','wb'))
-#pickle.dump(lm, open('model.pkl','wb'))
 
-# load the model to compare the results
-#model = pickle.load(open('model.pkl','rb'))
 
 
 
